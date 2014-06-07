b89e44920bc45781a965c58798ce23ae4b3f0e3b
Sergey Mironov <grrwlf@gmail.com>
2014-06-07 13:54:22 +0000

    Stress.sh: accept PORT as $1

--- stress.sh
+++ stress.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-URL="http://localhost:8080/Stress/viewsrc/.2E.2Ftest.2FTest6.2Eur"
+PORT=8080
+if test -n "$1" ; then
+  PORT=$1
+fi
+
+URL="http://localhost:$PORT/Stress/viewsrc/.2E.2Ftest.2FTest6.2Eur"
 
 for i in `seq 1 1 50` ; do 
 
